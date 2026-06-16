14b7cd2e49d488ae069c1916f4780b4b922f541c
Sanjana Awachat <sanjanaawachat1998@gmail.com>
2026-06-16 21:17:36 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { ServicesComponent } from './components/services/services.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'blog', component: BlogComponent },
+    { path: 'contact', component:ContactComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
